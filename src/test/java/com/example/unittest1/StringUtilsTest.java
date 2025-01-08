e115b3ef3387cc9fdd0fd04e2cdc01497a659a95
nqhuy <bit220080@st.cmcu.edu.vn>
2025-01-08 09:17:30 +0700

    more test case

--- src/test/java/com/example/unittest1/StringUtilsTest.java
+++ src/test/java/com/example/unittest1/StringUtilsTest.java
@@ -98,4 +98,112 @@ public class StringUtilsTest {
     public void testCountVowelsWithSpecialCharacters() {
         assertEquals(0, StringUtils.countVowels("@#$%^&*()"));
     }
+
+    // Tests for reverse method
+    @Test
+    public void testReverseWithSingleCharacter() {
+        assertEquals("a", StringUtils.reverse("a"));
+    }
+
+    @Test
+    public void testReverseWithSpacesOnly() {
+        assertEquals("   ", StringUtils.reverse("   "));
+    }
+
+    @Test
+    public void testReverseWithNumbers() {
+        assertEquals("321", StringUtils.reverse("123"));
+    }
+
+    @Test
+    public void testReverseWithAlphanumeric() {
+        assertEquals("321cba", StringUtils.reverse("abc123"));
+    }
+
+    @Test
+    public void testReverseWithSpecialAndAlphanumeric() {
+        assertEquals("!321cba@", StringUtils.reverse("@abc123!"));
+    }
+
+    @Test
+    public void testReverseWithLongString() {
+        String input = "abcdefghijklmnopqrstuvwxyz";
+        String expected = "zyxwvutsrqponmlkjihgfedcba";
+        assertEquals(expected, StringUtils.reverse(input));
+    }
+
+    // Tests for isPalindrome method
+    @Test
+    public void testIsPalindromeWithNumbers() {
+        assertTrue(StringUtils.isPalindrome("12321"));
+    }
+
+    @Test
+    public void testIsPalindromeWithSpaces() {
+        assertTrue(StringUtils.isPalindrome("  "));
+    }
+
+    @Test
+    public void testIsPalindromeWithNonPalindromeNumbers() {
+        assertFalse(StringUtils.isPalindrome("12345"));
+    }
+
+    @Test
+    public void testIsPalindromeWithUnicodeCharacters() {
+        assertTrue(StringUtils.isPalindrome("あいういあ"));
+    }
+
+    @Test
+    public void testIsPalindromeWithSpecialCharactersOnly() {
+        assertTrue(StringUtils.isPalindrome("!!@@!!"));
+    }
+
+    @Test
+    public void testIsPalindromeWithNonAlphanumericCharacters() {
+        assertTrue(StringUtils.isPalindrome("!a!"));
+    }
+
+    // Tests for countVowels method
+    @Test
+    public void testCountVowelsWithMixedCaseVowels() {
+        assertEquals(2, StringUtils.countVowels("hEllO"));
+    }
+
+    @Test
+    public void testCountVowelsWithNumbersAndVowels() {
+        assertEquals(5, StringUtils.countVowels("123aeiou123"));
+    }
+
+    @Test
+    public void testCountVowelsWithSpaces() {
+        assertEquals(5, StringUtils.countVowels("a e i o u"));
+    }
+
+    @Test
+    public void testCountVowelsWithSpecialCharactersAndVowels() {
+        assertEquals(5, StringUtils.countVowels("a!e@i#o$u%"));
+    }
+
+    @Test
+    public void testCountVowelsWithUnicodeCharacters() {
+        assertEquals(0, StringUtils.countVowels("你好世界")); // No vowels in these characters
+    }
+
+    @Test
+    public void testCountVowelsWithVietnameseCharacters() {
+        assertEquals(4, StringUtils.countVowels("Xin chào thế giới!"));
+    }
+
+    @Test
+    public void testCountVowelsWithRepeatingVowels() {
+        assertEquals(10, StringUtils.countVowels("aeiouaeiou"));
+    }
+
+    @Test
+    public void testCountVowelsWithLongString() {
+        String longString = "abcdefghijklmnopqrstuvwxyz" +
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "1234567890!@#$%^&*()";
+        assertEquals(10, StringUtils.countVowels(longString)); // 10 vowels in total
+    }
 }
